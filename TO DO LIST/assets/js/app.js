542f2897fa5e36aff03cd0db9e898bee99543b27
Up210063 <112641457+Up210063@users.noreply.github.com>
2024-03-15 23:09:22 -0600

    Update app.js

--- TO DO LIST/assets/js/app.js
+++ TO DO LIST/assets/js/app.js
@@ -1,33 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Elementos HTML
-    const userSelect = document.getElementById('select-users');
-    const userContainer = document.getElementById('user-container');
-    const displayButton = document.getElementById('btndisplay');
-
-    userSelect.addEventListener('change', () => {
-        const userId = userSelect.value;
-
-        fetch('informacion/usuarios.json')
-            .then(response => response.json())
-            .then(users => {
-                const selectedUser = users.find(user => user.id == userId);
-                updateUserInformation(selectedUser);
-            });
+function updateTaskList(tasks) {
+    const ul = taskContainer.querySelector('ul');
+    ul.innerHTML = ''; // Limpiar la lista antes de actualizarla
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `<span>${index + 1}. ${task.title}</span><input type="checkbox" ${task.completed ? 'checked' : ''}>`;
+        ul.appendChild(li);
     });
-
-    displayButton.addEventListener('click', () => {
-        toggleTaskContainerVisibility();
-    });
-
-    function updateUserInformation(user) {
-        const nombreCompleto = userContainer.querySelector('ul li:first-child span');
-        const emailUsuario = userContainer.querySelector('ul li:nth-child(2) span');
-        nombreCompleto.textContent = `${user.firstname} ${user.lastname}`;
-        emailUsuario.textContent = user.email;
-    }
-
-    function toggleTaskContainerVisibility() {
-        const taskContainer = document.getElementById('task-container');
-        taskContainer.style.visibility = taskContainer.style.visibility === 'visible' ? 'hidden' : 'visible';
-    }
-});
+}
