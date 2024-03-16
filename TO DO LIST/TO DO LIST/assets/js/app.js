0f80b2d09233ea0f003f98f333161cf78da10d4a
Up210063 <112641457+Up210063@users.noreply.github.com>
2024-03-15 23:24:22 -0600

    Add files via upload

---
+++ TO DO LIST/TO DO LIST/assets/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Elementos HTML
+    const userSelect = document.getElementById('select-users');
+    const userContainer = document.getElementById('user-container');
+    const taskContainer = document.getElementById('task-container');
+    const displayButton = document.getElementById('btndisplay');
+
+    userSelect.addEventListener('change', () => {
+        const userId = userSelect.value;
+
+        fetch('informacion/usuarios.json')
+            .then(response => response.json())
+            .then(users => {
+                const selectedUser = users.find(user => user.id == userId);
+                updateUserInformation(selectedUser);
+            });
+
+        fetch('informacion/tareas.json')
+            .then(response => response.json())
+            .then(tasks => {
+                const userTasks = tasks.filter(task => task.userId == userId);
+                updateTaskList(userTasks);
+            });
+    });
+
+    displayButton.addEventListener('click', () => {
+        toggleTaskContainerVisibility();
+    });
+
+    function updateUserInformation(user) {
+        const nombreCompleto = userContainer.querySelector('ul li:first-child span');
+        const emailUsuario = userContainer.querySelector('ul li:nth-child(2) span');
+        nombreCompleto.textContent = `${user.firstname} ${user.lastname}`;
+        emailUsuario.textContent = user.email;
+    }
+
+    function updateTaskList(tasks) {
+        const ul = taskContainer.querySelector('ul');
+        ul.innerHTML = ''; // Limpiar la lista antes de actualizarla
+        tasks.forEach((task, index) => {
+            const li = document.createElement('li');
+            li.innerHTML = `<span>${index + 1}. ${task.title}</span><input type="checkbox" ${task.completed ? 'checked' : ''}>`;
+            ul.appendChild(li);
+        });
+    }
+
+    function toggleTaskContainerVisibility() {
+        taskContainer.style.visibility = taskContainer.style.visibility === 'visible' ? 'hidden' : 'visible';
+    }
+});
